f4e0eb87865766c6951c48f8cd467da9548fe9fe
Arthaud <arthaudp33@gmail.com>
2024-01-19 00:05:49 +0100

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,7 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "prettier"],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
   },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx", "*.js"],
-      parser: "@typescript-eslint/parser",
-    },
-  ],
 };
