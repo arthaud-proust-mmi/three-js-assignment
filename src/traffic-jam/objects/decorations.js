217ef64e5ee1cde4bb044d13e3bf0cbdbd197166
Arthaud <arthaudp33@gmail.com>
2024-01-19 10:15:29 +0100

    perf: improve network load by caching objects

--- src/traffic-jam/objects/decorations.js
+++ src/traffic-jam/objects/decorations.js
@@ -15,8 +15,17 @@ const loadDecoration = async (carName) => {
   return carModel.scene;
 };
 
+const decorationsCache = {};
+const loadCachedDecoration = async (carName) => {
+  if (!decorationsCache[carName]) {
+    decorationsCache[carName] = await loadDecoration(carName);
+  }
+
+  return decorationsCache[carName].clone();
+};
+
 export const makeAcaciaTree = async () => {
-  const mesh = await loadDecoration("acacia-tree");
+  const mesh = await loadCachedDecoration("acacia-tree");
 
   const scale = 0.02;
   mesh.scale.set(scale, scale, scale);
@@ -25,7 +34,7 @@ export const makeAcaciaTree = async () => {
 };
 
 export const makeOakTree = async () => {
-  const mesh = await loadDecoration("oak-tree");
+  const mesh = await loadCachedDecoration("oak-tree");
 
   const scale = 4;
   mesh.scale.set(scale, scale, scale);
@@ -35,7 +44,7 @@ export const makeOakTree = async () => {
 };
 
 export const makeBloackOakTree = async () => {
-  const mesh = await loadDecoration("black-oak-tree");
+  const mesh = await loadCachedDecoration("black-oak-tree");
 
   const scale = 0.8;
   mesh.scale.set(scale, scale, scale);
@@ -45,7 +54,7 @@ export const makeBloackOakTree = async () => {
 };
 
 export const makeBlueEyedGrass = async () => {
-  const mesh = await loadDecoration("blue-eyed-grass");
+  const mesh = await loadCachedDecoration("blue-eyed-grass");
 
   const scale = 2;
   mesh.scale.set(scale, scale, scale);
